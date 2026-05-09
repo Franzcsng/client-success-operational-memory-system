fdb9a3adb0c99c4d155fd7265ff4fde6ae5cec49
Francis Causing <franzcsng23@gmail.com>
2026-05-09 23:51:01 +0800

    c4 - Create global components templates + main pages

---
+++ src/app/(dashboard)/layout.js
@@ -0,0 +1,20 @@
+import styles from './layout.module.scss'
+import Sidebar from '@/components/sidebar/Sidebar.js'
+import Header from '@/components/header/Header'
+import Footer from '@/components/footer/Footer'
+
+export default function DashboardLayout({children}){
+    return(
+        <div className={styles['dashboard-layout']}>
+            <Sidebar/>
+
+            <div className={styles['dashboard-layout-outlet']}>
+                <Header/>
+                <div className={styles['dashboard-outlet']}>
+                    {children}
+                </div>
+                <Footer/>
+            </div>
+        </div>
+    )
+}
