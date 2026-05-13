f8b942d3a9646e9f63abacbb8aa8aced831ba305
Francis Causing <franzcsng23@gmail.com>
2026-05-13 21:03:21 +0800

    c20 - Create footer with reset-demo button

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,24 @@
 import styles from './Footer.module.scss'
 
-export default function Footer(){
+export default function Footer({resetDemo}){
     return(
         <div className={styles.footer}>
+            <div className={styles['footer-child']}>
+                <p className={styles['logo-text']}>CS-OMS</p>
+            </div>
 
+            <div className={styles['footer-child']}>
+                <p className={styles['footer-info']}>FRANCIS NORMAN A. CAUSING</p>
+                <p className={styles['footer-info']}>franzcsng23@gmail.com</p>
+            </div>
+
+            <div className={styles['footer-child']}>
+                <button className={styles['reset-button']}
+                    onClick={()=>{
+                        resetDemo()
+                    }}
+                >Restart Demo</button>
+            </div>
         </div>
     )
 }
